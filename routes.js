3061b5acf7100e096bfb0755ba4dee7d84ba9c3b
Christian Pinto <pintochristian2@gmail.com>
2019-08-31 18:25:49 -0400

    Added a form to send the input response to the backend and redirect the user

--- routes.js
+++ routes.js
@@ -1,18 +1,32 @@
 const requestListener = (req,res) => {
-    let route = req.url;
+    const route = req.url;
+    const method = req.method
     if(route === '/'){
         res.write('<html>');
         res.write('<head><title>My First App</title></head>')
-        res.write('<body><h1>Welcome to my first Node.js application. You can also go to /users for another page</h1></body>')
+        res.write('<body><h1>Welcome to my first Node.js application. You can also go to /users for another page</h1><form action="/create-user" method="POST"><input type="text" placeholder="username" name="username"><button type="submit">Submit</button></form></body>')
         res.write('</html>')
         return res.end();
     }
-    else if(route === '/users'){
+    if(route === '/users'){
         res.write('<html>');
         res.write('<head><title>My First App</title></head>')
         res.write('<body><h1>Here is a list of users: <ul><li>John</li><li>Michael</li><li>Luis</li></ul></h1></body>')
         res.write('</html>')
         return res.end();
+    }
+    if(route === '/create-user' && method === 'POST'){
+        const body = [];
+        req.on('data', (data) => {
+            body.push(data)
+        })
+        req.on('end', () => {
+            const parsedData = Buffer.concat(body).toString();
+            console.log(parsedData.split("=")[1]);
+            res.statusCode = 302;
+            res.setHeader('Location' , '/')
+            res.end();
+        })
     }
 }
 
