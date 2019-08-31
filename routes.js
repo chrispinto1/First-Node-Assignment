319ef8561cfcfa0f3e2b88ee4698e3807ef3be53
Christian Pinto <pintochristian2@gmail.com>
2019-08-31 17:56:48 -0400

    second route /users

--- routes.js
+++ routes.js
@@ -1,10 +1,16 @@
 const requestListener = (req,res) => {
-    const users =['John', 'Michael'];
     let route = req.url;
     if(route === '/'){
         res.write('<html>');
         res.write('<head><title>My First App</title></head>')
         res.write('<body><h1>Welcome to my first Node.js application. You can also go to /users for another page</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    else if(route === '/users'){
+        res.write('<html>');
+        res.write('<head><title>My First App</title></head>')
+        res.write('<body><h1>Here is a list of users: <ul><li>John</li><li>Michael</li><li>Luis</li></ul></h1></body>')
         res.write('</html>')
         return res.end();
     }
